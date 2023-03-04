15b8e18950daf5586c744a64deed810a3ac5fea0
Nick-M1 <nicholasmalloura@gmail.com>
2023-03-04 11:50:12 +0000

    Search by text

--- lib/DB/both/allRecipeSortOptions.ts
+++ lib/DB/both/allRecipeSortOptions.ts
@@ -1,6 +1,6 @@
 export const allSortOptions: SortOptionsRecipeAndAuthor[] = [
     { name: 'Most Popular',        query: 'numberOfLikes',        order: "desc" },
     { name: 'Best Rating',         query: 'numberOfBookmarks',    order: "desc" },         //todo: give rating in comments
-    { name: 'Newest',              query: 'created_at',            order: "asc" },              //todo: save timestamp in recipeDB
+    { name: 'Newest',              query: 'created_at',           order: "desc" },
     { name: 'Quickest time',       query: 'cookTime',             order: "asc" },
 ]

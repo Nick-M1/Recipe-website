b602c4eac20474fad19b81c9d465d9757a95b65a
Nick-M1 <nicholasmalloura@gmail.com>
2023-03-16 21:45:39 +0000

    firebase querying

--- components/recipe/search/RecipesSearch.tsx
+++ components/recipe/search/RecipesSearch.tsx
@@ -21,15 +21,6 @@ type Props = {
 
 
 export default function RecipesSearch({ pagenumber, lastPageIndex, urlBasepath, currentUrlWithoutSort, currentSort, recipesAndAuthors, user, allCategories }: Props) {
-    if (recipesAndAuthors.length === 0)
-        return (
-            <div className="px-4 py-8 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-15">
-                <p className="text-3xl text-center text-gray-700">
-                    No recipes can be found for this query :(
-                </p>
-            </div>
-        );
-
     return (
         <>
             <div className="px-4 py-12 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-14">
@@ -39,7 +30,15 @@ export default function RecipesSearch({ pagenumber, lastPageIndex, urlBasepath, 
                     <div><DropdownComponent optionNames={allSortOptions} currentSort={currentSort} currentUrlWithoutSort={currentUrlWithoutSort} /></div>
                 </div>
 
-                <RecipeCardGrid recipesAndAuthors={recipesAndAuthors} quickview={true} user={user} flexGrid={true}/>
+                { recipesAndAuthors.length == 0 ? (
+                    <div className="px-4 pt-16 pb-64 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-15">
+                        <p className="text-3xl text-center text-gray-700">
+                            No recipes can be found for this query :(
+                        </p>
+                    </div>
+                ) : (
+                    <RecipeCardGrid recipesAndAuthors={recipesAndAuthors} quickview={true} user={user} flexGrid={true}/>
+                )  }
 
                 <div className='float-right pt-4'>
                     {/* todo: FIX URL WITH FUNCTION UTILS */}

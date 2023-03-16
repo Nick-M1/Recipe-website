c9ff6ff61f3f56826cb9419d7997f3ea3d01c201
Nick-M1 <nicholasmalloura@gmail.com>
2023-03-16 11:24:02 +0000

    added metadata

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -4,12 +4,7 @@ import Footer from "../../components/layouts/Footer";
 import {getServerSession} from "next-auth";
 import {authOptions} from "../../pages/api/auth/[...nextauth]";
 import getUserByEmail from "../../lib/DB/server/getUserByEmail";
-import {Metadata} from "next";
 import getAllRecipes from "../../lib/DB/server/getAllRecipes";
-
-// export const metadata: Metadata = {
-//     title: 'Home'
-// }
 
 export default async function Layout({children}: { children: React.ReactNode }) {
     const sessionAuth = await getServerSession(authOptions)
